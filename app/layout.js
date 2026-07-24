0f8666f096487173ade4b5725afdaf791f004867
CocaCola <41602086+MrZhongH@users.noreply.github.com>
2026-07-24 15:31:43 +0800

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,25 @@
+export const metadata = {
+  title: "Football AI Scout",
+  description: "全球足球分析系统",
+};
+
+
+export default function RootLayout({
+  children,
+}) {
+
+return (
+
+<html lang="zh-CN">
+
+<body>
+
+{children}
+
+</body>
+
+</html>
+
+);
+
+}
