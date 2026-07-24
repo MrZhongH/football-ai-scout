c6b3f87c25fb1390b353c82137aa650afbff0418
CocaCola <41602086+MrZhongH@users.noreply.github.com>
2026-07-24 14:20:49 +0800

    Update page.js

--- app/page.js
+++ app/page.js
@@ -5,48 +5,14 @@ import {useState} from "react";
 
 export default function Home(){
 
+
 const [keyword,setKeyword]=useState("");
 
-const [team,setTeam]=useState(null);
-
-const [loading,setLoading]=useState(false);
 
 
+function searchTeam(){
 
-async function searchTeam(){
-
-
-if(!keyword)return;
-
-
-setLoading(true);
-
-
-const res = await fetch(
-`/api/team-search?q=${encodeURIComponent(keyword)}`
-);
-
-
-const data = await res.json();
-
-
-
-console.log(data);
-
-
-
-if(data.response && data.response.length>0){
-
-setTeam(data.response[0].team);
-
-}else{
-
-setTeam(null);
-
-}
-
-
-setLoading(false);
+alert("按钮正常");
 
 
 }
@@ -55,32 +21,12 @@ setLoading(false);
 
 return (
 
-<main
-style={{
-padding:40,
-fontFamily:"Arial"
-}}
->
+<main style={{padding:40}}>
 
 
 <h1>
-⚽ 欢迎来到可乐的足球分析
+Football AI Scout V15.4 TEST
 </h1>
-
-
-<h2>
-Football AI Scout V15.4
-</h2>
-
-
-
-<hr/>
-
-
-<h2>
-🌍 全球球队搜索引擎
-</h2>
-
 
 
 <input
@@ -91,20 +37,11 @@ onChange={
 e=>setKeyword(e.target.value)
 }
 
-
-placeholder="输入球队，例如 皇马 曼城 拜仁"
-
-
 style={{
-
-padding:15,
-
-width:"70%"
-
+padding:15
 }}
 
 />
-
 
 
 <button
@@ -112,138 +49,16 @@ width:"70%"
 onClick={searchTeam}
 
 style={{
-
 padding:15,
-
 marginLeft:10
-
 }}
 
 >
-
-{
-loading?
-"搜索中..."
-:
-"搜索"
-}
-
+搜索
 </button>
-
-
-
-
-
-{
-
-team &&
-
-<div
-
-style={{
-
-marginTop:30,
-
-background:"#f5f7fb",
-
-padding:25,
-
-borderRadius:15
-
-}}
-
->
-
-
-<h2>
-🎯 搜索结果
-</h2>
-
-
-<img
-
-src={team.logo}
-
-width="120"
-
-/>
-
-
-
-<h2>
-
-{team.name}
-
-</h2>
-
-
-
-<p>
-国家：
-{team.country}
-</p>
-
-
-
-<p>
-成立：
-{team.founded}
-</p>
-
-
-
-<a
-
-href={`/team/${team.id}`}
-
->
-
-<button>
-
-进入球队分析
-
-</button>
-
-
-</a>
-
-
-
-</div>
-
-
-}
-
-
-
-
-<h2>
-📊 数据模块
-</h2>
-
-
-<ul>
-
-<li>球队信息</li>
-
-<li>Logo</li>
-
-<li>积分排名</li>
-
-<li>近期比赛</li>
-
-<li>历史交锋</li>
-
-<li>伤停信息</li>
-
-<li>球员阵容</li>
-
-</ul>
-
 
 
 </main>
-
 
 )
 
