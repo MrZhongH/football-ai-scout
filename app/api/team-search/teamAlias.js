65ae7727ec36a9a0ff87c11136b870867a7f7c71
CocaCola <41602086+MrZhongH@users.noreply.github.com>
2026-07-24 14:01:22 +0800

    Add teamAlias for football club translations

---
+++ app/api/team-search/teamAlias.js
@@ -0,0 +1,30 @@
+export const teamAlias = {
+
+"皇马":"Real Madrid",
+"皇家马德里":"Real Madrid",
+
+"巴萨":"Barcelona",
+"巴塞罗那":"Barcelona",
+
+"曼城":"Manchester City",
+"曼联":"Manchester United",
+
+"利物浦":"Liverpool",
+
+"阿森纳":"Arsenal",
+
+"切尔西":"Chelsea",
+
+"拜仁":"Bayern Munich",
+
+"多特":"Borussia Dortmund",
+
+"巴黎":"Paris Saint Germain",
+
+"尤文":"Juventus",
+
+"国际米兰":"Inter",
+
+"AC米兰":"AC Milan"
+
+};
