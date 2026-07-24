f1a7dcc0bee318d9d5a89ede1a040382ba9d705b
CocaCola <41602086+MrZhongH@users.noreply.github.com>
2026-07-24 15:02:34 +0800

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,65 +1,27 @@
 "use client";
 
-import {useState} from "react";
-
-
 export default function Home(){
 
-
-const [keyword,setKeyword]=useState("");
-
-
-
-function searchTeam(){
-
-alert("按钮正常");
-
-
-}
-
-
-
 return (
-
-<main style={{padding:40}}>
-
+<html>
+<body>
 
 <h1>
-Football AI Scout V15.4 TEST
+TEST
 </h1>
 
-
-<input
-
-value={keyword}
-
-onChange={
-e=>setKeyword(e.target.value)
-}
-
-style={{
-padding:15
-}}
-
-/>
-
-
 <button
-
-onClick={searchTeam}
-
-style={{
-padding:15,
-marginLeft:10
+onClick={()=>{
+console.log("clicked")
+alert("clicked")
 }}
-
 >
-搜索
+CLICK ME
 </button>
 
 
-</main>
-
+</body>
+</html>
 )
 
 }
