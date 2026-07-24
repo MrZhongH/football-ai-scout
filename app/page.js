3ab889a4727771270a4a4966847a8cd4ad52831e
CocaCola <41602086+MrZhongH@users.noreply.github.com>
2026-07-24 15:44:43 +0800

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,10 +1,30 @@
 "use client";
 
+import {useState} from "react";
+
 export default function Home(){
+
+const [msg,setMsg]=useState("等待点击");
+
+
+function test(){
+
+console.log("clicked");
+
+alert("React按钮正常运行");
+
+setMsg("点击成功");
+
+}
+
 
 return (
 
-<div>
+<main style={{
+padding:"40px",
+fontFamily:"Arial"
+}}>
+
 
 <h1>
 Football AI Scout V15.4 TEST
@@ -12,19 +32,22 @@ Football AI Scout V15.4 TEST
 
 
 <button
-
-onClick={()=>{
-alert("React OK")
+onClick={test}
+style={{
+padding:"15px 30px",
+fontSize:"20px"
 }}
-
 >
-
 CLICK
-
 </button>
 
 
-</div>
+<h2>
+{msg}
+</h2>
+
+
+</main>
 
 )
 
